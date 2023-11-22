7bc007ab11cac44cd37e523abd626e5c772881e3
SUNG PIL MOON <monspo1@gmail.com>
2023-11-22 11:03:04 -0500

    code cleaning in googleLogin.jsx

--- src/utils/GoogleLogin.jsx
+++ src/utils/GoogleLogin.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-// import { setCurLoggedInUser } from './../actions/action'
 import * as Google from 'expo-auth-session/providers/google';
 import { auth, signInWithCredential, GoogleAuthProvider } from "../utils/firebase";
 import { View } from 'react-native'
 import { Button } from 'react-native-paper';
-// import firebase from 'firebase';
-// import GoogleIcon from '@mui/icons-material/Google';
+import  { myGoogleConfig } from './../../credentials'
+// import { setCurLoggedInUser } from './../actions/action'
 
 function GoogleLogin(props) {
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: '510365340491-5p05hhoiib5cnm5enm7ln9mmo3abej1p.apps.googleusercontent.com',
+    clientId: myGoogleConfig.clientId,
   });
 
   const dispatch = useDispatch()
@@ -22,9 +21,9 @@ function GoogleLogin(props) {
       const credential = firebase.auth.GoogleAuthProvider.credential(id_token);
       signInWithCredential(auth, credential)
         .then((result) => {
-          console.log('result: ', result)
-          const { email, uid, displayName, photoURL } = result.user;
-          const loggedInUser = { email, uid, displayName, photoURL };
+          // console.log('result: ', result)
+          // const { email, uid, displayName, photoURL } = result.user;
+          // const loggedInUser = { email, uid, displayName, photoURL };
 
           // dispatch(setCurLoggedInUser(loggedInUser));
           // navigation.navigate('HomeScreen')
